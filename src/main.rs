bf4f98bf0b9eaddafeef684bea3885ed3d07cdbc
Jason Theiler <turret.jason@outlook.com>
2024-12-11 23:44:20 +0100

    feat: add context for errors during config parsing

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod config;
 mod deserializers;
 mod tmux;
 
+use anyhow::Context as _;
 use ratatui::crossterm::style::Stylize as _;
 
 use self::{
@@ -16,13 +17,13 @@ const APP_NAME: &str = env!("CARGO_PKG_NAME");
 fn main() {
     let args = Args::new();
     if let Err(err) = run(args) {
-        eprintln!("{} {err}", "error:".dark_red().bold());
+        eprintln!("{} {err:#}", "error:".dark_red().bold());
         std::process::exit(1);
     }
 }
 
 fn run(args: Args) -> anyhow::Result<()> {
-    let config = Config::new(&args)?;
+    let config = Config::new(&args).context("failed to parse configuration file")?;
     match args.command {
         Command::Create(args_create) => command::create(&config, &args_create),
         Command::Select => command::select(&config),
